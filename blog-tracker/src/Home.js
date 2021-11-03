75a08f004b8a6f1bbdbdc915f716b56777d6b9fe
tj on laptop <thomasjnix.dev@gmail.com>
2021-11-02 21:38:25 -0400

    Move to generic fetch file + destructure data from response for reusability

--- blog-tracker/src/Home.js
+++ blog-tracker/src/Home.js
@@ -1,46 +1,10 @@
 import React from 'react'
-import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    /**
-     * The useState method from React allows accessing state from the component
-     * In this example, useState accepts a value (that is assigned to the destructured name variable)
-     * and a function to update that value (that is assigned ot the destructured setName method)
-     */
-    const [blogs, setBlogs] = useState(null);
-    const [isLoading, setIsLoading] = useState(true); 
-    const [error, setError] = useState(null);
 
-    /**
-     * useEffect runs every time there is a re-render, or every time data changes
-     * When data is retrieved on this hook, it's called a side effect in React
-     * 
-     * By providing an array as the second argument, any properties provided will be 
-     * watched for updates and useEffect() will trigger on the updates of those values
-     */
-    useEffect(() => {
-        // Just to demo the loading behavior
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-        .then((res) => {
-            if (res.ok) {
-                return res.json();
-            } else {
-                throw new Error('Failed to fetch requested resources.');
-            }
-        })
-        .then((blogs) => {
-            setBlogs(blogs);
-            setIsLoading(false);
-            setError(null);
-        })
-        .catch((err) => {
-            setError(err.message);
-            setIsLoading(false);
-        })
-        }, 1000);
-    }, []);
+    const {data: blogs, isLoading, error} = useFetch('http://localhost:8000/blogs');
  
     return (
         <div className="home">
---
+++ blog-tracker/src/useFetch.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+const useFetch = (endpoint) => {
+    /**
+     * The useState method from React allows accessing state from the component
+     * In this example, useState accepts a value (that is assigned to the destructured name variable)
+     * and a function to update that value (that is assigned ot the destructured setName method)
+     */
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    /**
+     * useEffect runs every time there is a re-render, or every time data changes
+     * When data is retrieved on this hook, it's called a side effect in React
+     * 
+     * By providing an array as the second argument, any properties provided will be 
+     * watched for updates and useEffect() will trigger on the updates of those values
+     */
+    useEffect(() => {
+        // Just to demo the loading behavior
+        setTimeout(() => {
+            fetch(endpoint)
+                .then((res) => {
+                    if (res.ok) {
+                        return res.json();
+                    } else {
+                        throw new Error('Failed to fetch requested resources.');
+                    }
+                })
+                .then((data) => {
+                    setData(data);
+                    setIsLoading(false);
+                    setError(null);
+                })
+                .catch((err) => {
+                    setError(err.message);
+                    setIsLoading(false);
+                })
+        }, 1000);
+    }, []);
+
+    /**
+     * Return all of the data associated with the request
+     */
+    return {
+        data, isLoading, error
+    };
+}
+
+export default useFetch;
